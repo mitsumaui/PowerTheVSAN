ea41032dabe89c44f627c61c3c1a63ca108350e4
Jason Whitelock <jase@oshelp.co.uk>
2017-11-22 08:10:28 +0000

    Squashing Bugs after testing
    - Cleaned up duplicate code
    - Moved variable generation in Begin{} to Process{} so it can support passing of multiple clusters of different versions.

--- PowerTheVSAN.psm1
+++ PowerTheVSAN.psm1
@@ -1,73 +1,62 @@
 Function Get-VSANObjectHealth {
-    <#
-        .SYNOPSIS
-        Obtain object health for vSAN Cluster
-        .DESCRIPTION
-        This function performs an object health report for a vSAN Cluster
-        .PARAMETER VsanCluster
-        Specifies a vSAN Cluster object, returned by Get-Cluster cmdlet.
-        .EXAMPLE
-        PS C:\> Get-Cluster | Get-VSANObjectHealth
-    #>
+  <#
+      .SYNOPSIS
+      Obtain object health for vSAN Cluster
+      .DESCRIPTION
+      This function performs an object health report for a vSAN Cluster
+      .PARAMETER VsanCluster
+      Specifies a vSAN Cluster object, returned by Get-Cluster cmdlet.
+      .EXAMPLE
+      PS C:\> Get-Cluster | Get-VSANObjectHealth
+  #>
   
-    [CmdletBinding()]
-    Param (
-      [Parameter(Mandatory, ValueFromPipeline)]
-      [Alias("Cluster")]
-      [VMware.VimAutomation.ViCore.Types.V1.Inventory.Cluster]$VsanCluster
-      ,
-      [Parameter(Mandatory = $false)]
-      [switch]$ShowObjectUUIDs
-      ,
-      [Parameter(Mandatory = $false)]
-      [switch]$UseCachedInfo
-      ,
-      [Parameter(Mandatory = $false)]
-      [switch]$HealthyOnly
-      ,
-      [Parameter(Mandatory = $false)]
-      [ValidateSet("defaultView", "deployAssist")]
-      [string]$VsanHealthPerspective = "defaultView"
-    )
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [Alias("Cluster")]
+    [VMware.VimAutomation.ViCore.Types.V1.Inventory.Cluster]$VsanCluster
+    ,
+    [Parameter(Mandatory = $false)]
+    [switch]$ShowObjectUUIDs
+    ,
+    [Parameter(Mandatory = $false)]
+    [switch]$UseCachedInfo
+    ,
+    [Parameter(Mandatory = $false)]
+    [switch]$HealthyOnly
+    ,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("defaultView", "deployAssist")]
+    [string]$VsanHealthPerspective = "defaultView"
+  )
   
-    Begin {
-      $vcName = $vsancluster.Uid.Split("@")[1].Split(":")[0]
-      $vchs = Get-VsanView -Id "VsanVcClusterHealthSystem-vsan-cluster-health-system" -Server $vcName
-      $VsanVersion = (Get-VsanView -Id "VsanVcClusterHealthSystem-vsan-cluster-health-system" -Server $vcName).VsanVcClusterQueryVerifyHealthSystemVersions(($VsanCluster).Id) | select VcVersion
+  Begin {
+
+  }
+  
+  Process {
+    $vcName = $vsancluster.Uid.Split("@")[1].Split(":")[0]
+    $vchs = Get-VsanView -Id "VsanVcClusterHealthSystem-vsan-cluster-health-system" -Server $vcName
+    $VsanVersion = (Get-VsanView -Id "VsanVcClusterHealthSystem-vsan-cluster-health-system" -Server $vcName).VsanVcClusterQueryVerifyHealthSystemVersions(($VsanCluster).Id) | select VcVersion
+
+    if($VsanCluster.VsanEnabled -and $VsanVersion.VcVersion -lt '6.6') {
+      $result = $vchs.VsanQueryVcClusterHealthSummary($VsanCluster.id, $null, $null, $ShowObjectUUIDs, $null, $UseCachedInfo)
     }
-  
-    Process {
-      if($VsanCluster.VsanEnabled -and $VsanVersion.VcVersion -lt '6.6') {
-        $result = $vchs.VsanQueryVcClusterHealthSummary($VsanCluster.id, $null, $null, $ShowObjectUUIDs, $null, $UseCachedInfo)
-        if($result) {
-          if($HealthyOnly) {
-            $Health = $result.ObjectHealth.ObjectHealthDetail | Where-Object {$_.Health -notlike 'healthy' -and $_.NumObjects -gt 0}
-            if($Health) {
-              return $false
-            } else {
-              return $true
-            }
-          } else {
-            return $result.ObjectHealth.ObjectHealthDetail
-          }
+    elseif ($VsanCluster.VsanEnabled -and $VsanVersion.VcVersion -gt '6.6') {
+      $result = $vchs.VsanQueryVcClusterHealthSummary($VsanCluster.id, $null, $null, $ShowObjectUUIDs, $null, $UseCachedInfo, $VsanHealthPerspective)
+    }
+
+    if($result) {
+      if($HealthyOnly) {
+        $Health = $result.ObjectHealth.ObjectHealthDetail | Where-Object {$_.Health -notlike 'healthy' -and $_.NumObjects -gt 0}
+        if($Health) {
+          return $false
+        } else {
+          return $true
         }
-      }
-      elseif ($VsanCluster.VsanEnabled -and $VsanVersion.VcVersion -gt '6.6') {
-        $result = $vchs.VsanQueryVcClusterHealthSummary($VsanCluster.id, $null, $null, $ShowObjectUUIDs, $null, $UseCachedInfo, $VsanHealthPerspective)
-        if ($result) {
-            if ($HealthyOnly) {
-                $Health = $result.ObjectHealth.ObjectHealthDetail | Where-Object {$_.Health -notlike 'healthy' -and $_.NumObjects -gt 0}
-                if ($Health) {
-                    return $false
-                }
-                else {
-                    return $true
-                }
-            }
-            else {
-                return $result.ObjectHealth.ObjectHealthDetail
-            }
-        } 
+      } else {
+        return $result.ObjectHealth.ObjectHealthDetail
       }
     }
   }
+}
